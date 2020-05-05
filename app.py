d6a70f82db622a85db04415f1c0b688a62b581e1
Dana <30771887+DanaKlimova@users.noreply.github.com>
2020-05-05 18:50:28 +0300

    Add Post mode (#1)

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+from datetime import datetime
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(30), nullable=False, default="N/A")
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Post {self.id}"
 
 
 @app.route("/")
